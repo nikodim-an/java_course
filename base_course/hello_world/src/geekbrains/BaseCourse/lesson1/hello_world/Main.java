87fdbae1eac96fafa4053055b09ea1bb0124f62b
alexander_khilchenko <kh.git@mail.ru>
2021-08-31 19:50:33 +1000

    Начало изучения java

---
+++ base_course/hello_world/src/geekbrains/BaseCourse/lesson1/hello_world/Main.java
@@ -0,0 +1,8 @@
+package geekbrains.BaseCourse.lesson1.hello_world;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Здравствуй мир! я пришел учить java");
+        System.out.println("Судя по первому впечатлению — это крайне неудобный язык с тормазной IDE… пичаааль…");
+    }
+}
