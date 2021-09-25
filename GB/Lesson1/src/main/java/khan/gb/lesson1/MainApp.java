829bcc14a463ad2d63536815d04c12157be8970c
alexander_khilchenko <kh.git@mail.ru>
2021-09-25 17:52:26 +1000

    GB - домашняя работа №1

---
+++ GB/Lesson1/src/main/java/khan/gb/lesson1/MainApp.java
@@ -0,0 +1,22 @@
+package khan.gb.lesson1;
+
+/**
+ * первая программа на java (третья по счету.)
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        String message = "Хелловорлд";
+        final int warValue = 333; // Объявление константы
+        int x,y,z;
+        x=y=z=10;
+        System.out.println(x);
+        x=12;
+        System.out.println(y);
+        String m,n,e;
+        n=m=e="kls;djf ";
+        n=m;
+        System.out.println(n);
+        m= "Привет";
+        System.out.println(n);
+    }
+}
