18e72a0f41319727a3f3158e42cb2625322d680a
alexander_khilchenko <kh.git@mail.ru>
2021-09-30 19:56:33 +1000

    Логирование : переопределение форматтера (кастомный форматтер)

--- Stepik/Logging/src/main/java/ru/khan/logging/ToFile.java
+++ Stepik/Logging/src/main/java/ru/khan/logging/ToFile.java
@@ -2,6 +2,7 @@ package ru.khan.logging;
 
 import java.util.logging.*;
 import java.io.IOException;
+import java.util.Date;
 
 public class ToFile {
     static String message = "сообщение про сообщение в файле";
@@ -12,7 +13,20 @@ public class ToFile {
         try {
             //FileHandler fileHandler = new FileHandler("/home/alex/DeskTop/log.log", 5 * 1024 * 1024, 5); // пишет в несколько файлов и по достижении максимального размера начинает новый с новым номером
             FileHandler fileHandler = new FileHandler("/home/alex/DeskTop/log.log");
-            fileHandler.setFormatter(new SimpleFormatter()); // форматтер
+            //fileHandler.setFormatter(new SimpleFormatter()); // стандартный форматтер
+            // кастомный форматтер
+            fileHandler.setFormatter(new SimpleFormatter() {
+                private static final String format = "[%1$tF %1$tT] [%2$-7s] → %3$s %n";
+
+                @Override
+                public synchronized String format(LogRecord lr) {
+                    return String.format(format,
+                            new Date(lr.getMillis()),
+                            lr.getLevel().getLocalizedName(),
+                            lr.getMessage()
+                    );
+                }
+            });
             logger.addHandler(fileHandler);
             logger.setLevel(Level.ALL);
         } catch (Exception e) {
@@ -43,6 +57,8 @@ public class ToFile {
   <thread>1</thread>
   <message>Информационное сообщение : сообщение про сообщение в файле</message>
 </record>
-* если включить форматер - а в конслои он SimpleFormatter, то сообщения будут выглядеть как в консоли.
-*
+* если включить форматер - а в конслои он SimpleFormatter, то сообщения будут выглядеть как в консоли (в две строки).
+* в код включен кастомный форматтер, с записями в одну строку
+* пример:
+*  [2021-09-30 19:54:16] [INFO   ] → Информационное сообщение : сообщение про сообщение в файле
 * */
