2727f2686f5049df083b948a7a000a727d511127
alexander_khilchenko <kh.git@mail.ru>
2021-10-09 10:27:53 +1000

    GB, JAVA1, урок 4, реализация материала из методички

---
+++ GB/Lesson4/src/MainClass.java
@@ -0,0 +1,139 @@
+/**
+ * Пример игры в крестики нолики в процедурном стиле
+ *
+ * @author : ГИКБРАИНС
+ * @version : хер его знает, взято из методички
+ */
+
+/*
+Поскольку потом мнее ее дорабаотывать - отмечу, что тот кто начинает - имеет на один ход больше (тоесть он и заканчивает)
+на поле 3×3.
+для этого поля ключевой точкой является центр. если тот кто его не захватил (а ходит первым человек) ничья возможна
+только в том случае, если человек не захватил центр, а компьютер умеет блокировать ходы.
+ */
+
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainClass {
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
+    }
+
+    public static boolean checkWin(char symb) {
+        // проверка подебы одного из игроков
+        if(map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return
+                true;
+        if(map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return
+                true;
+        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return
+                true;
+        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) return
+                true;
+        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) return
+                true;
+        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return
+                true;
+        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return
+                true;
+        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return
+                true;
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void aiTurn() {
+        // типа интеллект программы
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y +
+                1));
+        map[y][x] = DOT_O;
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
