b5c789bd146739d18dcfef7851330f4d4bb002f8
alexander_khilchenko <kh.git@mail.ru>
2021-11-30 13:03:25 +1000

    Формы в swing

--- swing/src/learn/Forms.java
+++ swing/src/learn/Forms.java
@@ -14,7 +14,16 @@ import javax.swing.*;
 
 public class Forms {
 
+    static class myExendedForm extends JFrame {
+        public myExendedForm(){
+            setVisible(true);
+            setExtendedState(6); // работает только если окно видимо
+        }
+    }
+
     static class myForm extends JFrame {
+        // если здесь унаследоваться от JWindow то у окна не будет заголовка
+        // и рамки
         public myForm(){
             setBounds(10,10,300,70);
             setTitle("Заголовок второго окна");
@@ -37,6 +46,6 @@ public class Forms {
     public static void main(String[] args) {
         simpleForm();               // минимальный код формы «на лету»
         myForm form = new myForm(); // более правильная реализация формы
+        myExendedForm form1= new myExendedForm(); // maximized форма
     }
-
 }
