afc20ba794f407599e8fc5809ad3ffb2df265592
alexander_khilchenko <kh.git@mail.ru>
2021-12-15 13:30:38 +1000

    коллекторы

---
+++ Stepik/StreamAPI/src/Collectors_example.java
@@ -0,0 +1,18 @@
+/**
+ * Класс Collectors_example
+ *
+ * @author : Хильченко А.Н
+ * @project : StreamAPI
+ * @date : 15.12.2021
+ * @comments : Класс примеров работы с коллекторами
+ */
+
+import java.util.*;
+
+public class Collectors_example {
+
+    public static void main(String[] args) {
+
+    }
+
+}
