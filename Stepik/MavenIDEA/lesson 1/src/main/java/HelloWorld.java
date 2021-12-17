68d9c9a667e96839943cf1555da8b93e89720abb
alexander_khilchenko <kh.git@mail.ru>
2021-12-17 14:50:33 +1000

    Maven : урок № 1

---
+++ Stepik/MavenIDEA/lesson 1/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Класс HelloWorld
+ *
+ * @author : Хильченко А.Н
+ * @project : lesson 1
+ * @date : 17.12.2021
+ * @comments :
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
